63cd05c0a3447760936d7d279c1243f848d9a502
chanmi24 <chanmi24@gmail.com>
2026-05-06 18:43:36 +0900

    feat(schedule): 학사일정 조회 API (#45)

--- academic-service/src/main/java/com/green/academic/application/schedule/ScheduleRepository.java
+++ academic-service/src/main/java/com/green/academic/application/schedule/ScheduleRepository.java
@@ -2,6 +2,7 @@ package com.green.academic.application.schedule;
 
 import com.green.academic.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+public interface ScheduleRepository extends JpaRepository<Schedule, Long>, JpaSpecificationExecutor<Schedule> {
 }
