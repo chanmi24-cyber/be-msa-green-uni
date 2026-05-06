eda64bf9bfc5a53c380ca502a6b0a02bb8fddd41
ksy <tndud4615@gmail.com>
2026-05-06 13:19:30 +0900

    feat(common) building enum 추가

--- core-service/src/main/java/com/green/core/entity/lecture/Classroom.java
+++ core-service/src/main/java/com/green/core/entity/lecture/Classroom.java
@@ -1,5 +1,6 @@
 package com.green.core.entity.lecture;
 import com.green.common.entity.CreatedUpdatedAt;
+import com.green.common.enumcode.EnumBuilding;
 import io.hypersistence.utils.hibernate.id.Tsid;
 import jakarta.persistence.*;
 import lombok.*;
@@ -15,8 +16,9 @@ public class Classroom{
     @Column(name = "room_id")
     private Long roomId;
 
+    @Convert(converter = EnumBuilding.CodeConverter.class)
     @Column(name = "building", nullable = false, length = 30)
-    private String building;
+    private EnumBuilding building;
 
     @Column(name = "room", nullable = false, length = 10)
     private String room;
