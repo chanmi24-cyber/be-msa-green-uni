012c7e2d725832c27cc662082851ed2371833958
ksy <tndud4615@gmail.com>
2026-05-08 12:40:51 +0900

    refactor(auth) loginRes에 회원 정보 제거

--- auth-service/src/main/java/com/green/auth/application/auth/AuthController.java
+++ auth-service/src/main/java/com/green/auth/application/auth/AuthController.java
@@ -36,8 +36,6 @@ public class AuthController {
 
         LoginRes resultData = LoginRes.builder()
                 .memberCode( loginMember.getMemberCode( ))
-                .name( null )
-                .major( null )
                 .deviceId( jwtMember.getDeviceId() )
                 .role(loginMember.getRole().getCode())
                 .isFirstLogin(loginMember.getIsFirstLogin())
