bf6a9092860f8f854a1e0212528753c89a5bd88b
chanmi24 <chanmi24@gmail.com>
2026-05-08 22:55:55 +0900

    Revert "Merge pull request #80 from green-uni/76-lecture-list"
    This reverts commit ebac415cef5c733145d3d435f6f95c75fc6261bd, reversing
    changes made to 082ef31d2b0ac24d34cf0f1c5b30c1514a6f989b.
    
    # Conflicts:
    #	core-service/src/main/java/com/green/core/application/lecture/LectureController.java
    #	core-service/src/main/java/com/green/core/application/lecture/LectureService.java
    #	core-service/src/main/java/com/green/core/application/lecture/ProfessorLectureController.java
    #	core-service/src/main/java/com/green/core/application/lecture/StudentLectureController.java
    #	core-service/src/main/java/com/green/core/application/lecture/mapper/LectureMapper.java
    #	core-service/src/main/resources/mappers/LectureMapper.xml

--- core-service/src/main/java/com/green/core/application/lecture/AdminLectureController.java
+++ core-service/src/main/java/com/green/core/application/lecture/AdminLectureController.java
@@ -3,13 +3,9 @@ package com.green.core.application.lecture;
 import com.green.common.auth.MemberContext;
 import com.green.common.model.MemberDto;
 import com.green.common.model.ResultResponse;
-import com.green.core.application.lecture.model.AdminLectureReq;
 import com.green.core.application.lecture.model.LectureApprovalReq;
-import com.green.core.application.lecture.model.MyLectureListRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,14 +21,4 @@ public class AdminLectureController {
                 .message("강의 승인 상태 변경 성공")
                 .build();
     }
-
-    @GetMapping("/my")
-    public ResultResponse<List<MyLectureListRes>> getAdminLectures(@ModelAttribute AdminLectureReq req) {
-        System.out.println("req: " + req); // 찍어보기
-        return ResultResponse.<List<MyLectureListRes>>builder()
-                .message("강의 승인 관리 목록 조회 성공")
-                .data(lectureService.getAdminLectures(req))
-                .build();
-    }
-
 }
