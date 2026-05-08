0dfcd4a407972f8763fcffc4dedd23f0cc54816b
chanmi24 <chanmi24@gmail.com>
2026-05-08 22:56:38 +0900

    Reapply "Merge pull request #80 from green-uni/76-lecture-list"
    This reverts commit cc52f2dbbd128248a84537cbdf8e0631a989c53c.

--- auth-service/src/main/java/com/green/auth/AuthApplication.java
+++ auth-service/src/main/java/com/green/auth/AuthApplication.java
@@ -1,8 +1,11 @@
 package com.green.auth;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = {"com.green.common", "com.green.auth"})
