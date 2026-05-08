7a36c23f183c56613537e82ee08a90919c2b78bb
chanmi24 <chanmi24@gmail.com>
2026-05-08 23:26:05 +0900

    feat(lecture):강의수정및삭제

--- core-service/src/main/java/com/green/core/application/lecture/repository/LectureScheduleRepository.java
+++ core-service/src/main/java/com/green/core/application/lecture/repository/LectureScheduleRepository.java
@@ -1,7 +1,9 @@
 package com.green.core.application.lecture.repository;
 
+import com.green.core.entity.lecture.Lecture;
 import com.green.core.entity.lecture.LectureSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LectureScheduleRepository extends JpaRepository<LectureSchedule, Long> {
+    void deleteAllByLecture(Lecture lecture);
 }
