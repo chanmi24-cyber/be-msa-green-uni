30ea99f445276d6718ffd49975aa7679019a3981
ksy <tndud4615@gmail.com>
2026-05-15 17:32:10 +0900

    fix(auth) 이메일 인증 후 비밀번호 변경시 redis 저장 및 삭제 관리

--- auth-service/src/main/java/com/green/auth/application/email/model/EmailSendReq.java
+++ auth-service/src/main/java/com/green/auth/application/email/model/EmailSendReq.java
@@ -2,11 +2,13 @@ package com.green.auth.application.email.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class EmailSendReq {
+    @NotNull
     private Long memberCode;
-    @Email
+    @NotBlank @Email
     private String email;
 }
