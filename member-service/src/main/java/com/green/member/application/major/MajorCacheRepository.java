ad1a3114e3ec28c46f362fd4450ba70f68990bae
ksy <tndud4615@gmail.com>
2026-05-16 13:04:31 +0900

    feat(member) 단과대 목록 조회 API 추가

--- member-service/src/main/java/com/green/member/application/major/MajorCacheRepository.java
+++ member-service/src/main/java/com/green/member/application/major/MajorCacheRepository.java
@@ -2,9 +2,13 @@ package com.green.member.application.major;
 
 import com.green.member.entity.cache.MajorCache;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MajorCacheRepository extends JpaRepository<MajorCache, Long> {
     List<MajorCache> findByActive(String active);
+
+    @Query("SELECT DISTINCT m.collegeName FROM MajorCache m WHERE m.active = 'RUNNING'")
+    List<String> findDistinctCollegeNames();
 }
