e91f9a513ebb84b395daa3586361fa9d7f02d40d
chanmi24 <chanmi24@gmail.com>
2026-05-18 22:33:35 +0900

    fix(lecture):에러코드수정

--- core-service/src/main/java/com/green/core/application/lecture/LectureService.java
+++ core-service/src/main/java/com/green/core/application/lecture/LectureService.java
@@ -13,6 +13,7 @@ import com.green.core.application.major.MajorRepository;
 import com.green.core.entity.lecture.*;
 import com.green.core.entity.major.Major;
 import com.green.core.exception.LectureErrorCode;
+import com.green.core.exception.MajorErrorCode;
 import com.green.core.scheduleValidator.SchedulePeriodValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -45,7 +46,7 @@ public class LectureService {
         schedulePeriodValidator.checkCourseOpen();
 
         Major major = majorRepository.findById(req.getMajorId())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 학과입니다."));
+                .orElseThrow(() -> new BusinessException(MajorErrorCode.MAJOR_NOT_FOUND));
 
         Lecture lecture = Lecture.builder()
                 .memberCode(memberDto.memberCode())  // MemberDto record라서 ()로 호출
@@ -67,7 +68,7 @@ public class LectureService {
 
         for (LectureCreateReq.ScheduleReq s : req.getSchedules()) {
             Classroom classroom = classroomRepository.findById(s.getRoomId())
-                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 강의실입니다."));
+                    .orElseThrow(() -> new BusinessException(LectureErrorCode.CLASSROOM_NOT_FOUND));
 
             // 수용인원 체크
             if (req.getMaxStd() > classroom.getCapacity()) {
@@ -208,13 +209,13 @@ public class LectureService {
 
         //수정로직
         Major major = majorRepository.findById(req.getMajorId())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 학과입니다."));
+                .orElseThrow(() -> new BusinessException(MajorErrorCode.MAJOR_NOT_FOUND));
 
         // 기존 스케줄 삭제 후 재등록
         lectureScheduleRepository.deleteAllByLecture(lecture);
         for (LectureDetailReq.ScheduleReq s : req.getSchedules()) {
             Classroom classroom = classroomRepository.findById(s.getRoomId())
-                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 강의실입니다."));
+                    .orElseThrow(() -> new BusinessException(LectureErrorCode.CLASSROOM_NOT_FOUND));
             LectureSchedule schedule = LectureSchedule.builder()
                     .lecture(lecture)
                     .classRoom(classroom)
