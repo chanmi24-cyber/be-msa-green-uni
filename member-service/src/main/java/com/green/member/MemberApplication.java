f1de800701485857d159cd1915e807e405f8d3fb
chanmi24-cyber <chanmi24@gmail.com>
2026-05-26 16:21:17 +0900

    fix(noti):import삭제

--- member-service/src/main/java/com/green/member/MemberApplication.java
+++ member-service/src/main/java/com/green/member/MemberApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan(basePackages = {"com.green.member", "com.green.common"})
 @EnableJpaAuditing
