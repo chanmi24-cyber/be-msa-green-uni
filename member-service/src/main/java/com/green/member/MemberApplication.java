c9d130825500525f8309c4b8cea6ca859169aabb
chanmi24-cyber <chanmi24@gmail.com>
2026-06-16 16:32:26 +0900

    docs: swagger 사용법 시연(memberController)

--- member-service/src/main/java/com/green/member/MemberApplication.java
+++ member-service/src/main/java/com/green/member/MemberApplication.java
@@ -3,6 +3,8 @@ package com.green.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @ConfigurationPropertiesScan
 @SpringBootApplication(scanBasePackages = {"com.green.member", "com.green.common"})
